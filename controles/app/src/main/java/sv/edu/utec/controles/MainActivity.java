1d7f050add35782311f741d32dca8f2acd9ecdd1
José David Ramírez Hernández <47959020+JoseRamirezhz@users.noreply.github.com>
2023-02-20 22:21:45 -0600

    inicial
    controle

--- controles/app/src/main/java/sv/edu/utec/controles/MainActivity.java
+++ controles/app/src/main/java/sv/edu/utec/controles/MainActivity.java
@@ -3,6 +3,7 @@ package sv.edu.utec.controles;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -17,5 +18,21 @@ EditText edtValor, edtResultado;
         //union parte grafica con parte de codigo
         edtValor = findViewById(R.id.edtTabla);
         edtResultado = findViewById(R.id.edtMtabla);
+    }
+
+    public void Tabla(View v){
+        int valtabla = Integer.parseInt(edtValor.getText().toString());
+        int resultados;
+        String Resultado = "";
+        String titulo = "";
+        edtResultado.setText(Resultado);
+        titulo = "Tabla:";
+        edtResultado.setText(titulo+""+valtabla+"\n");
+        for(int i=0; i<=10;i++){
+            resultados = valtabla*i;
+            Resultado = String.valueOf(resultados);
+            //edtResultado.setText(Resultado);
+            edtResultado.append(valtabla+"*"+i+"="+Resultado+"\n"); // appen va agregando
+        }
     }
 }
