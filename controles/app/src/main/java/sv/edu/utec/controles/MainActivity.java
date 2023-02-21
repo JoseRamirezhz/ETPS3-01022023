523fbbf0aa73a40467cf4138dc8ec709bca56d78
José David Ramírez Hernández <47959020+JoseRamirezhz@users.noreply.github.com>
2023-02-20 21:47:35 -0600

    200220231937

--- controles/app/src/main/java/sv/edu/utec/controles/MainActivity.java
+++ controles/app/src/main/java/sv/edu/utec/controles/MainActivity.java
@@ -3,12 +3,19 @@ package sv.edu.utec.controles;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+EditText edtValor, edtResultado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //union parte grafica con parte de codigo
+        edtValor = findViewById(R.id.edtTabla);
+        edtResultado = findViewById(R.id.edtMtabla);
     }
 }
