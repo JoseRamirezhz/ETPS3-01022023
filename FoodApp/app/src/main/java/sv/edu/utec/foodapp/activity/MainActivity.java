df0ed2e13f8085928ff6504b50f3c98444c89e03
José David Ramírez Hernández <47959020+JoseRamirezhz@users.noreply.github.com>
2023-03-06 23:36:59 -0600

    FoodApp

---
+++ FoodApp/app/src/main/java/sv/edu/utec/foodapp/activity/MainActivity.java
@@ -0,0 +1,29 @@
+package sv.edu.utec.foodapp.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import sv.edu.utec.foodapp.R;
+
+public class MainActivity extends AppCompatActivity {
+    ConstraintLayout btnInicial;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnInicial = findViewById(R.id.btnInicio);
+
+        //entorno para ejecutar ese boton funcionalidad
+        btnInicial.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), Principal.class));
+            }
+        });
+    }
+}
