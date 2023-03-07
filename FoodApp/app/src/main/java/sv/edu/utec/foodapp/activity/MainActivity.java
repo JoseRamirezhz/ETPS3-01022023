2c8754ce464f882398dcfcd7fd0885e28bc546f2
José David Ramírez Hernández <47959020+JoseRamirezhz@users.noreply.github.com>
2023-03-06 23:39:51 -0600

    Update MainActivity.java

--- FoodApp/app/src/main/java/sv/edu/utec/foodapp/activity/MainActivity.java
+++ FoodApp/app/src/main/java/sv/edu/utec/foodapp/activity/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
         btnInicial.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(getApplicationContext(), Principal.class));
+                startActivity(new Intent(getApplicationContext(), Principal.class)); 
             }
         });
     }
