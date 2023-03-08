126654bf45a21f2a83ef30729d56fb9be19f4f8e
José David Ramírez Hernández <47959020+JoseRamirezhz@users.noreply.github.com>
2023-03-07 23:35:05 -0600

    FootApp

--- FoodApp/app/src/main/java/sv/edu/utec/foodapp/activity/Principal.java
+++ FoodApp/app/src/main/java/sv/edu/utec/foodapp/activity/Principal.java
@@ -3,14 +3,23 @@ package sv.edu.utec.foodapp.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import sv.edu.utec.foodapp.R;
 
 public class Principal extends AppCompatActivity {
-
+    TextView tvUserMsj;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+        tvUserMsj = findViewById(R.id.tvUsuario);
+        //bundle es una clase que permite traer las intenciones
+
+        Bundle bundle = getIntent().getExtras();
+        String info = "Hola, "+ bundle.getString("usuario");
+        tvUserMsj.setText(info);
+
     }
 }
